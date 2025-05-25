766fc071e1803444a44376f8277103206057ee5c
UshanBandara <99163337+UshanIBandara@users.noreply.github.com>
2025-05-25 23:12:51 +0530

    updated

---
+++ page/reports/consumables/AgeAnalysisReport/total.blade.php
@@ -0,0 +1,12 @@
+<h1> Buddhist and Pali University of SriLanka </h1>
+<h3> Age Analysis - Consumable Items </h3>
+
+<br />
+
+<div class="col-md-12 text-right">
+
+    <a href="{{ url('/reports/conItem/AgeAnalysisReport/exportToExcelAll/' . $fromDate . '/' . $toDate) }}"
+        class="btn btn-success"> Download as Excel</a>
+</div>
+
+</div>
